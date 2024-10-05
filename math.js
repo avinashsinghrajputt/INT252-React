50fa7a28aa641bb37d4b5e8cca54e7d7b66f8afc
avinashrajputt1 <avinashsinghrathore1206@gmail.com>
2024-10-05 22:49:02 +0530

    up

--- math.js
+++ math.js
@@ -1,5 +1,13 @@
 const add = (a, b) => a + b;
 const sub = (a, b) => a - b;
+const mul = (a, b) => a * b;
+const div = (a, b) => {
+  if (b === 0) {
+    throw new Error("Division by zero is not allowed");
+  }
+  return a / b;
+};
+const pow = (a, b) => Math.pow(a, b);
 
-//expirting the function it can be used in another file
-export { add, sub };
+// Exporting the functions so they can be used in another file
+export { add, sub, mul, div, pow };
